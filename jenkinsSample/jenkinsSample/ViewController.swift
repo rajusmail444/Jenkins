3054bf701d09d0bb25013aacb11f96fa68af43fb
rajusmail444 <rajusmail444@gmail.com>
2017-05-18 22:30:23 +0800

    for Jenkins

---
+++ jenkinsSample/jenkinsSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jenkinsSample
+//
+//  Created by Rajesh Billakanti on 18/05/17.
+//  Copyright © 2017 RAjaY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
